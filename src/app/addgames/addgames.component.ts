2d8d7b8463c0d398d9452865952700c2e148fbd3
gauri23092002 <gauri23092002@gmail.com>
2022-07-06 19:13:24 +0530

    games added

---
+++ src/app/addgames/addgames.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-addgames',
+  templateUrl: './addgames.component.html',
+  styleUrls: ['./addgames.component.css']
+})
+export class AddgamesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
