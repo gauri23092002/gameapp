df6fa087c0c80d7b689ca59e0a320384d2118d14
gauri23092002 <gauri23092002@gmail.com>
2022-07-06 19:47:22 +0530

    games viewed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddgamesComponent } from './addgames/addgames.component';
+import { ViewgamesComponent } from './viewgames/viewgames.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AddgamesComponent
+    AddgamesComponent,
+    ViewgamesComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/viewgames/viewgames.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-viewgames',
+  templateUrl: './viewgames.component.html',
+  styleUrls: ['./viewgames.component.css']
+})
+export class ViewgamesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
